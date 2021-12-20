5e25a6b4d44f0f0544e0fe5ff684a3f99887bafd
Tiffeny <tiffeny@pm.com>
2021-12-20 09:18:55 +0100

    First commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import { customAlphabet } from 'nanoid';
+import { lowercase } from 'nanoid-dictionary';
+
+const lowercaseRandomString = customAlphabet(lowercase, 10);
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="container mx-auto">
+        <h1 className="text-7xl font-bold text-black tracking-tighter">
+          Modern Password Generator
+        </h1>
+
+        <input type="text" className="border-2 border-black shadow-custom-input focus:outline-none py-1 px-2" placeholder="Mot de passe" />
+
+        <button className="bg-black text-white py-1 px-4 block">Generate</button>
+        <button className="bg-black text-white py-1 px-4 block">Copy</button>
+      </div>
     </div>
   );
 }
---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  darkMode: 'class',
+  content: [
+    "./src/**/*.{js,jsx}",
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        'inter': ['Inter', 'sans-serif']
+      },
+      boxShadow: {
+        'custom-input' : '4px 4px 0px 0px #000',
+      }
+    },
+  },
+  plugins: [],
+}
